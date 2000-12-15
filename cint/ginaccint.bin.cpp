fe54232fa5da4efde501ab967de1f7e33c57e5c9
Richard Kreckel <Richard.Kreckel@uni-mainz.de>
2000-12-15 20:46:29 +0000

    - Makefile.am: target clean should remove G__*. - ginaccint.bin.cpp: check for recent enough cint.

--- cint/ginaccint.bin.cpp
+++ cint/ginaccint.bin.cpp
@@ -3,8 +3,8 @@
 
 #include "G__ci.h"   /* G__atpause is defined in G__ci.h */
 
-#if (!defined(G__CINTVERSION)) || (G__CINTVERSION < 501439)
-#error You need at least cint 5.14.39 to compile GiNaC-cint. Download it via http from root.cern.ch/root/Cint.html or via ftp from ftpthep.physik.uni-mainz.de/pub/cint
+#if (!defined(G__CINTVERSION)) || (G__CINTVERSION < 501460)
+#error You need at least cint 5.14.60 to compile GiNaC-cint. Download it via http from root.cern.ch/root/Cint.html or via ftp from ftpthep.physik.uni-mainz.de/pub/cint
 #endif // (!defined(G__CINTVERSION)) || (G__CINTVERSION < 501438)
 
 #include <iostream>
