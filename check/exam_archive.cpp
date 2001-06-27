eaaba68453c2863a12244a532db4455d1fc41a7a
Christian Bauer <Christian.Bauer@uni-mainz.de>
2001-06-27 00:25:55 +0000

    adjusted for the fact that "A + c" is now rejected by add::eval if A is noncommutative and c is a numeric

--- check/exam_archive.cpp
+++ check/exam_archive.cpp
@@ -48,8 +48,7 @@ unsigned exam_archive(void)
 	  + dirac_gamma(varidx(mu, dim)) * dirac_gamma(varidx(mu, 4-dim, true))
 	    * color_T(idx(x, 8), 1) * color_h(idx(x, 8), idx(y, 8), idx(2, 8))
 	    * indexed(x, sy_anti(), idx(2*y+1, x), varidx(-mu, 5))
-	  - 2 * spinor_metric(spinidx(x), spinidx(y))
-	  + 0.775;
+	  - 2.4275 * spinor_metric(spinidx(x), spinidx(y));
 
 	archive ar;
 	ar.archive_ex(e, "expr 1");
