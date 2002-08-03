2dcbb3a4955e294d62b45c3db8bdccb7d6fdf833
Christian Bauer <Christian.Bauer@uni-mainz.de>
2002-08-03 19:20:22 +0000

    updated the autogen.sh script

--- autogen.sh
+++ autogen.sh
@@ -7,19 +7,17 @@ DIE=0
 
 PROG=GiNaC
 
+# Check how echo works in this /bin/sh
+case `echo -n` in
+-n) _echo_n=   _echo_c='\c';;
+*)  _echo_n=-n _echo_c=;;
+esac
+
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have autoconf installed to compile $PROG."
         echo "Download the appropriate package for your distribution,"
         echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-        DIE=1
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-        echo
-        echo "You must have automake installed to compile $PROG."
-        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.4.tar.gz"
-        echo "(or a newer version if it is available)"
         DIE=1
 }
 
@@ -31,22 +29,51 @@ PROG=GiNaC
         DIE=1
 }
 
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+        echo
+        echo "You must have automake installed to compile $PROG."
+        echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.4.tar.gz"
+        echo "(or a newer version if it is available)"
+        DIE=1
+}
+
+(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "**Error**: Missing aclocal. The version of automake"
+	echo "installed doesn't appear recent enough."
+	echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
 if test "$DIE" -eq 1; then
         exit 1
 fi
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
+aclocalinclude="$ACLOCAL_FLAGS"; \
+(echo $_echo_n " + Running aclocal: $_echo_c"; \
+    aclocal $aclocalinclude; \
+ echo "done.") && \
+(echo $_echo_n " + Running libtoolize: $_echo_c"; \
+    libtoolize --force --copy >/dev/null 2>&1; \
+ echo "done.") && \
+(echo $_echo_n " + Running autoheader: $_echo_c"; \
+    autoheader; \
+ echo "done.") && \
+(echo $_echo_n " + Running automake: $_echo_c"; \
+    automake --gnu --add-missing --copy; \
+ echo "done.") && \
+(echo $_echo_n " + Running autoconf: $_echo_c"; \
+    autoconf; \
+ echo "done.")
+
+rm -f config.cache
+
+if [ x"$NO_CONFIGURE" = "x" ]; then
+    echo " + Running 'configure $@':"
+    if [ -z "$*" ]; then
+	echo "   ** If you wish to pass arguments to ./configure, please"
+        echo "   ** specify them on the command line."
+    fi
+    ./configure "$@"
 fi
-
-for dir in .
-do 
-  echo processing $dir
-  (cd $dir; \
-  aclocalinclude="$ACLOCAL_FLAGS"; \
-  aclocal $aclocalinclude; \
-  autoheader; libtoolize -c; automake -a -c; autoconf)
-done
-
-./configure "$@"
