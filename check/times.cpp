20e3b69938ba04116a09e98baa8d9fbe18c382f5
Richard Kreckel <Richard.Kreckel@uni-mainz.de>
2003-10-12 14:18:49 +0000

    * times.cpp (randomify_symbol_serials): New.

--- check/times.cpp
+++ check/times.cpp
@@ -23,8 +23,35 @@
 #include <stdexcept>
 #include "times.h"
 
+/** Generate a random amount of symbols and destroy them again immediatly.
+ *  This operation effectively makes the serial numbers of all subsequent
+ *  symbols unpredictable.  If the serials are unpredictable, then so are
+ *  their hash values.  If the hash values are unpredictable, then so are
+ *  the canonical orderings.  If the canonical orderings are unpredictable,
+ *  all subsequent times are subject to some variation.  This variation,
+ *  however is natural and desireable for two reasons: First, we cannot know
+ *  how many symbols have been generated before in real world computations.
+ *  Second, the following timings are subject to some semi-random variation
+ *  anyways because short timings need to be repeated until enough time has
+ *  gone by for the measurement to be reliable.  During this process the serial
+ *  numbers will be shifted anyways in a semi-random way.  It is better not
+ *  to lull the user in a false sense of reproducibility and instead confront
+ *  her with the normal variation to be expected.
+ */
+void randomify_symbol_serials()
+{
+	srand((unsigned)time(NULL));
+	const int m = rand() % 666;
+	for (int s=0; s<m; ++s ) {
+		symbol* tmp = new symbol;
+		delete tmp;
+	}
+}
+
 int main()
 {
+	randomify_symbol_serials();
+
 	unsigned result = 0;
 	
 #define TIME(which) \
