c73eae02da30a989fbbb160a15e390269e69b255
Jens Vollinga <vollinga@thep.physik.uni-mainz.de>
2004-01-04 16:51:08 +0000

    Added log(exp(x)) -> x for real symbols.

--- ginac/inifcns_trans.cpp
+++ ginac/inifcns_trans.cpp
@@ -124,6 +124,9 @@ static ex log_eval(const ex & x)
 	// log(exp(t)) -> t (if -Pi < t.imag() <= Pi):
 	if (is_ex_the_function(x, exp)) {
 		const ex &t = x.op(0);
+		if (is_a<symbol>(t) && (ex_to<symbol>(t).get_domain() == symbol_options::real)) {
+			return t;
+		}
 		if (t.info(info_flags::numeric)) {
 			const numeric &nt = ex_to<numeric>(t);
 			if (nt.is_real())
