AlexeiSheplyakov/GiNaC

commit 45b1e47372090352ac5af655b32473df2abab23b

Implement modular multivariate GCD (based on chinese remaindering algorithm).
Both algorithm and implementation are a bit inefficient:

* algorithm does not make use of sparseness of inputs (and most of
  multivariate polynomials are quite sparse)
* GiNaC's expressions are essentially immutable. Thus some simple
  operations (i.e. multiplying a polynomial by an element of the base ring)
  are prohibitively expensive.
* All numbers (i.e. GiNaC::numeric objects) are heap allocated.

Still it's much faster (~5x on bivariate polynomials, ~100x on 3-variate
ones) than (subresultant) PRS algorithm, so gcd() uses modular algorithm
by default.

ginac/polynomial/collect_vargs.cpp
@@ -1,0 +1,164 @@
+#include <iterator>
+#include <map>
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+#include <ginac/ginac.h>
+#include "collect_vargs.h"
+#include <cln/integer.h>
+#include "smod_helpers.h"
+#include "debug.hpp"
+
+namespace GiNaC
+{
+
+typedef std::map<exp_vector_t, ex> ex_collect_priv_t;
+
+static void 
+collect_vargs(ex_collect_priv_t& ec, ex e, const exvector& vars);
+static void
+collect_term(ex_collect_priv_t& ec, const ex& e, const exvector& vars);
+static void wipe_out_zeros(ex_collect_priv_t& ec);
+
+template<typename T, typename CoeffCMP>
+struct compare_terms
+{
+	const CoeffCMP& coeff_cmp;
+	explicit compare_terms(const CoeffCMP& coeff_cmp_) : coeff_cmp(coeff_cmp_)
+	{ }
+	inline bool operator()(const T& t1, const T& t2) const
+	{
+		bool exponent_is_less =
+			std::lexicographical_compare(t1.first.rbegin(),
+						     t1.first.rend(),
+						     t2.first.rbegin(),
+						     t2.first.rend());
+		if (exponent_is_less)
+			return true;
+
+		if ((t1.first == t2.first) &&
+				coeff_cmp(t2.second, t2.second))
+			return true;
+		return false;
+	}
+};
+
+template<typename T, typename CoeffCMP>
+static struct compare_terms<T, CoeffCMP>
+make_compare_terms(const T& dummy, const CoeffCMP& coeff_cmp)
+{
+	return compare_terms<T, CoeffCMP>(coeff_cmp);
+}
+
+void collect_vargs(ex_collect_t& ec, const ex& e, const exvector& vars)
+{
+	ex_collect_priv_t ecp;
+	collect_vargs(ecp, e, vars);
+	ec.reserve(ecp.size());
+	std::copy(ecp.begin(), ecp.end(), std::back_inserter(ec));
+	std::sort(ec.begin(), ec.end(),
+		  make_compare_terms(*ec.begin(), ex_is_less()));
+}
+
+static void 
+collect_vargs(ex_collect_priv_t& ec, ex e, const exvector& vars)
+{
+	e = e.expand();
+	if (e.is_zero()) {
+		ec.clear();
+		return;
+	}
+
+	if (!is_a<add>(e)) {
+		collect_term(ec, e, vars);
+		return;
+	}
+
+	for (const_iterator i = e.begin(); i != e.end(); ++i)
+		collect_term(ec, *i, vars);
+
+	wipe_out_zeros(ec);
+}
+
+static void
+collect_term(ex_collect_priv_t& ec, const ex& e, const exvector& vars)
+{
+	if (e.is_zero())
+		return;
+	static const ex ex1(1);
+	exp_vector_t key(vars.size());
+	ex pre_coeff = e;
+	for (std::size_t i = 0; i < vars.size(); ++i) {
+		const int var_i_pow = pre_coeff.degree(vars[i]);
+		key[i] = var_i_pow;
+		pre_coeff = pre_coeff.coeff(vars[i], var_i_pow);
+	}
+	ex_collect_priv_t::iterator i = ec.find(key);
+	if (i != ec.end())
+		i->second += pre_coeff;
+	else
+		ec.insert(ex_collect_priv_t::value_type(key, pre_coeff));
+}
+
+static void wipe_out_zeros(ex_collect_priv_t& m)
+{
+	ex_collect_priv_t::iterator i = m.begin();
+	while (i != m.end()) {
+		// be careful to not invalide iterator, use post-increment
+		// for that, see e.g.
+		// http://coding.derkeiler.com/Archive/C_CPP/comp.lang.cpp/2004-02/0502.html
+		if (i->second.is_zero())
+			m.erase(i++);
+		else
+			++i;
+	}
+}
+
+GiNaC::ex
+ex_collect_to_ex(const ex_collect_t& ec, const GiNaC::exvector& vars)
+{
+	exvector ev;
+	ev.reserve(ec.size());
+	for (std::size_t i = 0; i < ec.size(); ++i) {
+		exvector tv;
+		tv.reserve(vars.size() + 1);
+		for (std::size_t j = 0; j < vars.size(); ++j) {
+			if (ec[i].first[j] != 0)
+				tv.push_back(power(vars[j], ec[i].first[j]));
+		}
+		tv.push_back(ec[i].second);
+		ex tmp = (new mul(tv))->setflag(status_flags::dynallocated);
+		ev.push_back(tmp);
+	}
+	ex ret = (new add(ev))->setflag(status_flags::dynallocated);
+	return ret;
+}
+
+ex lcoeff_wrt(ex e, const exvector& x)
+{
+	static const ex ex0(0);
+	e = e.expand();
+	if (e.is_zero())
+		return ex0;
+
+	ex_collect_t ec;
+	collect_vargs(ec, e, x);
+	return ec.rbegin()->second;
+}
+
+cln::cl_I integer_lcoeff(const ex& e, const exvector& vars)
+{
+	ex_collect_t ec;
+	collect_vargs(ec, e, vars);
+	if (ec.size() == 0)
+		return cln::cl_I(0);
+	ex lc = ec.rbegin()->second;
+	bug_on(!is_a<numeric>(lc), "leading coefficient is not an integer");
+	bug_on(!lc.info(info_flags::integer),
+		"leading coefficient is not an integer");
+
+	return to_cl_I(lc);
+}
+
+} // namespace GiNaC
+

ginac/polynomial/divide_in_z_p.cpp
@@ -1,0 +1,88 @@
+#include <ginac/ginac.h>
+#include "smod_helpers.h"
+
+namespace GiNaC
+{
+/** 
+ * Exact polynomial division of a, b \in Z_p[x_0, \ldots, x_n]
+ * It doesn't check whether the inputs are proper polynomials, so be careful
+ * of what you pass in.
+ *  
+ * @param a  first multivariate polynomial (dividend)
+ * @param b  second multivariate polynomial (divisor)
+ * @param q  quotient (returned)
+ * @param var variables X iterator to first element of vector of symbols
+ *
+ * @return "true" when exact division succeeds (the quotient is returned in
+ *          q), "false" otherwise.
+ * @note @a p = 0 means the base ring is Z
+ */
+bool divide_in_z_p(const ex &a, const ex &b, ex &q, const exvector& vars, const long p)
+{
+	static const ex _ex1(1);
+	if (b.is_zero())
+		throw(std::overflow_error("divide_in_z: division by zero"));
+	if (b.is_equal(_ex1)) {
+		q = a;
+		return true;
+	}
+	if (is_exactly_a<numeric>(a)) {
+		if (is_exactly_a<numeric>(b)) {
+			// p == 0 means division in Z
+			if (p == 0) {
+				const numeric tmp = ex_to<numeric>(a/b);
+				if (tmp.is_integer()) {
+					q = tmp;
+					return true;
+				} else
+					return false;
+			} else {
+				q = (a*recip(ex_to<numeric>(b), p)).smod(numeric(p));
+				return true;
+			}
+		} else
+			return false;
+	}
+	if (a.is_equal(b)) {
+		q = _ex1;
+		return true;
+	}
+
+	// Main symbol
+	const ex &x = vars.back();
+
+	// Compare degrees
+	int adeg = a.degree(x), bdeg = b.degree(x);
+	if (bdeg > adeg)
+		return false;
+
+	// Polynomial long division (recursive)
+	ex r = a.expand();
+	if (r.is_zero())
+		return true;
+	int rdeg = adeg;
+	ex eb = b.expand();
+	ex blcoeff = eb.coeff(x, bdeg);
+	exvector v;
+	v.reserve(std::max(rdeg - bdeg + 1, 0));
+	exvector rest_vars(vars);
+	rest_vars.pop_back();
+	while (rdeg >= bdeg) {
+		ex term, rcoeff = r.coeff(x, rdeg);
+		if (!divide_in_z_p(rcoeff, blcoeff, term, rest_vars, p))
+			break;
+		term = (term*power(x, rdeg - bdeg)).expand();
+		v.push_back(term);
+		r = (r - term*eb).expand();
+		if (p != 0)
+			r = r.smod(numeric(p));
+		if (r.is_zero()) {
+			q = (new add(v))->setflag(status_flags::dynallocated);
+			return true;
+		}
+		rdeg = r.degree(x);
+	}
+	return false;
+}
+
+}

ginac/polynomial/divide_in_z_p.h
@@ -1,0 +1,27 @@
+#ifndef GINAC_CHINREM_GCD_DIVIDE_IN_Z_P_H
+#define GINAC_CHINREM_GCD_DIVIDE_IN_Z_P_H
+
+#include <ginac/ginac.h>
+namespace GiNaC
+{
+
+/** 
+ * Exact polynomial division of a, b \in Z_p[x_0, \ldots, x_n]
+ * It doesn't check whether the inputs are proper polynomials, so be careful
+ * of what you pass in.
+ *  
+ * @param a  first multivariate polynomial (dividend)
+ * @param b  second multivariate polynomial (divisor)
+ * @param q  quotient (returned)
+ * @param var variables X iterator to first element of vector of symbols
+ *
+ * @return "true" when exact division succeeds (the quotient is returned in
+ *          q), "false" otherwise.
+ */
+extern bool
+divide_in_z_p(const ex &a, const ex &b, ex &q, const exvector& vars, const long p);
+
+} // namespace GiNaC
+
+#endif /* GINAC_CHINREM_GCD_DIVIDE_IN_Z_P_H */
+

ginac/polynomial/euclid_gcd_wrap.h
@@ -1,0 +1,56 @@
+#ifndef GINAC_PGCD_EUCLID_GCD_H
+#define GINAC_PGCD_EUCLID_GCD_H
+#include "upoly.hpp"
+#include "gcd_euclid.tcc"
+#include "smod_helpers.h"
+#include <ginac/ginac.h>
+#include "debug.hpp"
+
+namespace GiNaC
+{
+
+static void ex2upoly(umodpoly& u, ex e, const ex& var, const long p)
+{
+	e = e.expand();
+	cln::cl_modint_ring R = cln::find_modint_ring(cln::cl_I(p));
+	u.resize(e.degree(var) + 1);
+	for (int i = 0; i <= e.degree(var); ++i) {
+		ex ce = e.coeff(var, i);
+		bug_on(!is_a<numeric>(ce), "i = " << i << ", " <<
+			"coefficient is not a number: " << ce);
+		const cln::cl_I c = to_cl_I(ce);
+		u[i] = R->canonhom(c);
+	}
+}
+
+static ex umodpoly2ex(const umodpoly& a, const ex& var, const long p)
+{
+	cln::cl_modint_ring R = cln::find_modint_ring(cln::cl_I(p));
+	const numeric pnum(p);
+	exvector ev(a.size());
+	for (std::size_t i = a.size(); i-- != 0; ) {
+		const cln::cl_I c = smod(R->retract(a[i]), p);
+		const ex term = numeric(c)*power(var, i);
+		ev.push_back(term);
+	}
+	ex ret = (new add(ev))->setflag(status_flags::dynallocated);
+	return ret;
+}
+	
+static ex euclid_gcd(ex A, ex B, const ex& var, const long p)
+{
+	A = A.expand();
+	B = B.expand();
+
+	umodpoly a, b;
+	ex2upoly(a, A, var, p);
+	ex2upoly(b, B, var, p);
+	umodpoly g;
+	gcd_euclid(g, a, b);
+	ex ge = umodpoly2ex(g, var, p);
+	return ge;
+}
+
+} // namespace GiNaC
+
+#endif

ginac/polynomial/eval_point_finder.h
@@ -1,0 +1,51 @@
+#ifndef GINAC_PGCD_EVAL_POINT_FINDER_H
+#define GINAC_PGCD_EVAL_POINT_FINDER_H
+#include <ginac/ginac.h>
+#include <set>
+
+namespace GiNaC
+{
+
+/// Find a `good' evaluation point b \in Z_p for a pair of multivariate
+/// polynomials A, B \in Z_p[x_n][x_0, \ldots, x_n]. Here `good' means
+/// that b is not a root of GCD of contents of A and B. N.B. content
+/// is univariate polynomial \in Z_p[x_n]
+struct eval_point_finder
+{
+	typedef long value_type;
+	const value_type p;
+	std::set<value_type> points;
+	const random_modint modint_generator;
+	bool operator()(value_type& b, const ex& g, const ex& x);
+	eval_point_finder(const value_type& p_) : p(p_), modint_generator(p)
+	{ }
+};
+
+bool eval_point_finder::operator()(value_type& b, const ex& lc, const ex& x)
+{
+	random_modint modint_generator(p);
+	// Search for a new element of field
+	while (points.size() < p - 1) {
+		value_type b_ = modint_generator();
+		// check if this evaluation point was already used
+		if (points.find(b_) != points.end())
+			continue;
+
+		// mark found value as used, even if it's a root of lc
+		// (so we don't need to do the check once more)
+		points.insert(b_);
+		// Now make sure it's NOT the root of GCD's leading coeffient
+		if (lc.subs(x == numeric(b_)).smod(numeric(p)).is_zero())
+			continue;
+		// Nice, it's our next evaluation point
+		b = b_;
+		return true;
+	}
+	// All possible evaluation points were used.
+	return false;
+}
+
+}
+
+#endif /* GINAC_PGCD_EVAL_POINT_FINDER_H */
+

ginac/polynomial/mgcd.cpp
@@ -1,0 +1,96 @@
+#include "chinrem_gcd.h"
+#include <cln/integer.h>
+#include "pgcd.h"
+#include "collect_vargs.h"
+#include "primes_factory.h"
+#include "divide_in_z_p.h"
+#include "poly_cra.h"
+
+namespace GiNaC
+{
+
+static cln::cl_I extract_integer_content(ex& Apr, const ex& A)
+{
+	static const cln::cl_I n1(1);
+	const numeric icont_ = A.integer_content();
+	const cln::cl_I icont = cln::the<cln::cl_I>(icont_.to_cl_N());
+	if (icont != 1) {
+		Apr = (A/icont_).expand();
+		return icont;
+	} else {
+		Apr = A;
+		return n1;
+	}
+}
+
+ex chinrem_gcd(const ex& A_, const ex& B_, const exvector& vars)
+{
+	ex A, B;
+	const cln::cl_I a_icont = extract_integer_content(A, A_);
+	const cln::cl_I b_icont = extract_integer_content(B, B_);
+	const cln::cl_I c = cln::gcd(a_icont, b_icont);
+
+	const cln::cl_I a_lc = integer_lcoeff(A, vars);
+	const cln::cl_I b_lc = integer_lcoeff(B, vars);
+	const cln::cl_I g_lc = cln::gcd(a_lc, b_lc);
+
+	const ex& x(vars.back());
+	int n = std::min(A.degree(x), B.degree(x));
+	const cln::cl_I A_max_coeff = to_cl_I(A.max_coefficient()); 
+	const cln::cl_I B_max_coeff = to_cl_I(B.max_coefficient());
+	const cln::cl_I lcoeff_limit = (cln::cl_I(1) << n)*cln::abs(g_lc)*
+		std::min(A_max_coeff, B_max_coeff);
+
+	cln::cl_I q = 0;
+	ex H = 0;
+
+	long p;
+	primes_factory pfactory;
+	while (true) {
+		bool has_primes = pfactory(p, g_lc);
+		if (!has_primes)
+			throw chinrem_gcd_failed();
+
+		const numeric pnum(p);
+		ex Ap = A.smod(pnum);
+		ex Bp = B.smod(pnum);
+		ex Cp = pgcd(Ap, Bp, vars, p);
+
+		const cln::cl_I g_lcp = smod(g_lc, p); 
+		const cln::cl_I Cp_lc = integer_lcoeff(Cp, vars);
+		const cln::cl_I nlc = smod(recip(Cp_lc, p)*g_lcp, p);
+		Cp = (Cp*numeric(nlc)).expand().smod(pnum);
+		int cp_deg = Cp.degree(x);
+		if (cp_deg == 0)
+			return numeric(g_lc);
+		if (zerop(q)) {
+			H = Cp;
+			n = cp_deg;
+			q = p;
+		} else {
+			if (cp_deg == n) {
+				ex H_next = chinese_remainder(H, q, Cp, p);
+				q = q*cln::cl_I(p);
+				H = H_next;
+			} else if (cp_deg < n) {
+				// all previous homomorphisms are unlucky
+				q = p;
+				H = Cp;
+				n = cp_deg;
+			} else {
+				// dp_deg > d_deg: current prime is bad
+			}
+		}
+		if (q < lcoeff_limit)
+			continue; // don't bother to do division checks
+		ex C, dummy1, dummy2;
+		extract_integer_content(C, H);
+		if (divide_in_z_p(A, C, dummy1, vars, 0) && 
+				divide_in_z_p(B, C, dummy2, vars, 0))
+			return (numeric(c)*C).expand();
+		// else: try more primes
+	}
+}
+
+} // namespace GiNaC
+

ginac/Makefile.am
@@ -35,6 +35,24 @@ polynomial/interpolate_padic_uvar.h \
 polynomial/sr_gcd_uvar.h \
 polynomial/heur_gcd_uvar.h \
 polynomial/gcd_uvar.cpp \
+polynomial/chinrem_gcd.cpp \
+polynomial/chinrem_gcd.h \
+polynomial/collect_vargs.cpp \
+polynomial/collect_vargs.h \
+polynomial/divide_in_z_p.cpp \
+polynomial/divide_in_z_p.h \
+polynomial/euclid_gcd_wrap.h \
+polynomial/eval_point_finder.h \
+polynomial/mgcd.cpp \
+polynomial/newton_interpolate.h \
+polynomial/optimal_vars_finder.cpp \
+polynomial/optimal_vars_finder.h \
+polynomial/pgcd.cpp \
+polynomial/pgcd.h \
+polynomial/poly_cra.h \
+polynomial/primes_factory.h \
+polynomial/primpart_content.cpp \
+polynomial/smod_helpers.h \
 polynomial/debug.hpp
 
 libginac_la_LDFLAGS = -version-info $(LT_VERSION_INFO) -release $(LT_RELEASE)

ginac/normal.cpp
@@ -44,6 +44,7 @@
 #include "pseries.h"
 #include "symbol.h"
 #include "utils.h"
+#include "polynomial/chinrem_gcd.h"
 
 namespace GiNaC {
 
@@ -1623,8 +1624,15 @@ ex gcd(const ex &a, const ex &b, ex *ca, ex *cb, bool check_args, unsigned optio
 		}
 #endif
 	}
+	if (options & gcd_options::use_sr_gcd) {
+		g = sr_gcd(aex, bex, var);
+	} else {
+		exvector vars;
+		for (std::size_t n = sym_stats.size(); n-- != 0; )
+			vars.push_back(sym_stats[n].sym);
+		g = chinrem_gcd(aex, bex, vars);
+	}
 
-	g = sr_gcd(aex, bex, var);
 	if (g.is_equal(_ex1)) {
 		// Keep cofactors factored if possible
 		if (ca)

commit b9dc3d71083fe1a2f90a34db24b57b06ad51ebde

Fixed compile errors introduced in previous commit 45b1e47372090352ac5af655b32473df2abab23b.

ginac/polynomial/collect_vargs.cpp
@@ -3,7 +3,10 @@
 #include <algorithm>
 #include <stdexcept>
 #include <string>
-#include <ginac/ginac.h>
+#include "add.h"
+#include "mul.h"
+#include "operators.h"
+#include "power.h"
 #include "collect_vargs.h"
 #include <cln/integer.h>
 #include "smod_helpers.h"

ginac/polynomial/divide_in_z_p.cpp
@@ -1,4 +1,6 @@
-#include <ginac/ginac.h>
+#include "add.h"
+#include "operators.h"
+#include "power.h"
 #include "smod_helpers.h"
 
 namespace GiNaC

ginac/polynomial/divide_in_z_p.h
@@ -1,7 +1,7 @@
 #ifndef GINAC_CHINREM_GCD_DIVIDE_IN_Z_P_H
 #define GINAC_CHINREM_GCD_DIVIDE_IN_Z_P_H
 
-#include <ginac/ginac.h>
+#include "ex.h"
 namespace GiNaC
 {
 

ginac/polynomial/euclid_gcd_wrap.h
@@ -3,7 +3,11 @@
 #include "upoly.hpp"
 #include "gcd_euclid.tcc"
 #include "smod_helpers.h"
-#include <ginac/ginac.h>
+#include "add.h"
+#include "ex.h"
+#include "operators.h"
+#include "power.h"
+#include "relational.h"
 #include "debug.hpp"
 
 namespace GiNaC

ginac/polynomial/eval_point_finder.h
@@ -1,6 +1,6 @@
 #ifndef GINAC_PGCD_EVAL_POINT_FINDER_H
 #define GINAC_PGCD_EVAL_POINT_FINDER_H
-#include <ginac/ginac.h>
+#include "operators.h"
 #include <set>
 
 namespace GiNaC

ginac/polynomial/mgcd.cpp
@@ -1,3 +1,4 @@
+#include "operators.h"
 #include "chinrem_gcd.h"
 #include <cln/integer.h>
 #include "pgcd.h"