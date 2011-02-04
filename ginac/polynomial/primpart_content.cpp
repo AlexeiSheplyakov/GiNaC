286b0d93528e35957a50c51343e0b4a9bc623bce
Richard Kreckel <kreckel@ginac.de>
2011-02-04 08:59:28 +0100

    Extend copyright to 2011.

--- ginac/polynomial/primpart_content.cpp
+++ ginac/polynomial/primpart_content.cpp
@@ -3,7 +3,7 @@
  *  Function to find primitive part of a multivariate polynomial. */
 
 /*
- *  GiNaC Copyright (C) 1999-2010 Johannes Gutenberg University Mainz, Germany
+ *  GiNaC Copyright (C) 1999-2011 Johannes Gutenberg University Mainz, Germany
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
