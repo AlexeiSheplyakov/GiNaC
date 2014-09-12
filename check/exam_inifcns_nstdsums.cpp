AlexeiSheplyakov/GiNaC

commit 97ef604e3da2e918cad6cb10af61f7d07999ed97

G_numeric: fix numeric evaluation with trailing zeros and y != 1.
Make sure G_numeric makes use of scaling relation (G_do_hoelder) only if
there are no traling zeros. Use G_do_trafo otherwise (which can handle
trailing zeros).

ginac/inifcns_nstdsums.cpp
@@ -1136,6 +1136,7 @@ G_numeric(const std::vector<cln::cl_N>& x, const std::vector<int>& s,
 	// check for convergence and necessary accelerations
 	bool need_trafo = false;
 	bool need_hoelder = false;
+	bool have_trailing_zero = false;
 	std::size_t depth = 0;
 	for (std::size_t i = 0; i < x.size(); ++i) {
 		if (!zerop(x[i])) {
@@ -1149,14 +1150,16 @@ G_numeric(const std::vector<cln::cl_N>& x, const std::vector<int>& s,
 				need_hoelder = true;
 		}
 	}
-	if (zerop(x[x.size() - 1]))
+	if (zerop(x[x.size() - 1])) {
+		have_trailing_zero = true;
 		need_trafo = true;
+	}
 
 	if (depth == 1 && x.size() == 2 && !need_trafo)
 		return - Li_projection(2, y/x[1], cln::float_format(Digits));
 	
 	// do acceleration transformation (hoelder convolution [BBB])
-	if (need_hoelder)
+	if (need_hoelder && !have_trailing_zero)
 		return G_do_hoelder(x, s, y);
 	
 	// convergence transformation

commit 9e80b0d339d1ce83f51e0eb5fb101c41f23f6a71

G_numeric: fix evaluation with y == 1
Do apply Hoelder convolution if y == 1 even if there are trailing zeros.
Fixes the regression introduced by commit 97ef604e ("G_numeric: fix
numeric evaluation with trailing zeros and y != 1.")

check/exam_inifcns_nstdsums.cpp
@@ -365,6 +365,33 @@ static unsigned inifcns_test_legacy()
 	return result;
 }
 
+static unsigned check_G_y_one_bug()
+{
+	exvector exprs;
+	exprs.push_back(G(lst(-1,-1, 1,-1, 0), 1));
+	exprs.push_back(G(lst(-1, 0, 1,-1, 0), 1));
+	exprs.push_back(G(lst(-1, 1,-1,-1, 0), 1));
+	exprs.push_back(G(lst(-1, 1,-1, 0, 0), 1));
+	exprs.push_back(G(lst(-1, 1,-1, 1, 0), 1));
+	exprs.push_back(G(lst(-1, 1, 0,-1, 0), 1));
+	exprs.push_back(G(lst(-1, 1, 1,-1, 0), 1));
+	exprs.push_back(G(lst( 0,-1, 1,-1, 0), 1));
+	exprs.push_back(G(lst( 0, 1, 1,-1, 0), 1));
+	unsigned err = 0;
+	for (exvector::const_iterator ep = exprs.begin(); ep != exprs.end(); ++ep) {
+		try {
+			ex val = ep->evalf();
+			if (!is_a<numeric>(val)) {
+				clog << "evalf(" << *ep << ") is not a number: " << val << endl;
+				++err;
+			}
+		} catch (std::exception& oops) {
+			clog << "evalf(" << *ep << "): got an exception" << oops.what() << endl;
+			++err;
+		}
+	}
+	return err;
+}
 
 unsigned exam_inifcns_nstdsums(void)
 {
@@ -377,6 +404,7 @@ unsigned exam_inifcns_nstdsums(void)
 	result += inifcns_test_HLi();
 	result += inifcns_test_LiG();
 	result += inifcns_test_legacy();
+	result += check_G_y_one_bug();
 	
 	return result;
 }

ginac/inifcns_nstdsums.cpp
@@ -1149,16 +1149,19 @@ G_numeric(const std::vector<cln::cl_N>& x, const std::vector<int>& s,
 				need_hoelder = true;
 		}
 	}
-	if (zerop(x[x.size() - 1])) {
-		have_trailing_zero = true;
+	have_trailing_zero = zerop(x.back());
+	if (have_trailing_zero) {
 		need_trafo = true;
+		if (y != 1) {
+			need_hoelder = false;
+		}
 	}
 
 	if (depth == 1 && x.size() == 2 && !need_trafo)
 		return - Li_projection(2, y/x[1], cln::float_format(Digits));
 	
 	// do acceleration transformation (hoelder convolution [BBB])
-	if (need_hoelder && !have_trailing_zero)
+	if (need_hoelder)
 		return G_do_hoelder(x, s, y);
 	
 	// convergence transformation